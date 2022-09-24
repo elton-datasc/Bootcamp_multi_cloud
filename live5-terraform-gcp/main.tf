4e0da0715cdfab73a253931bbc4cce9ab2616240
Elton <elton-guilherme@hotmail.com>
2022-09-24 19:10:49 -0300

    resourses

--- live5-terraform-gcp/main.tf
+++ live5-terraform-gcp/main.tf
@@ -1,13 +1,38 @@
 terraform {
-  required_providers{
+  required_providers {
     google = {
       source = "hashicorp/google"
-      version = "4.37.0"
+      version = "~> 4.37.0"
     }
   }
 }
 
 provider "google" {
-  project = var.gcp_project_id
+    project = var.gcp_project_id
+}
 
+resource "google_container_cluster" "primary" {
+  name               = "kubernetes-cluster-001"
+  location           = var.gcp_region
+  initial_node_count = 1
+  enable_autopilot = true
+  ip_allocation_policy {
+  }
+}
+
+
+resource "google_sql_database_instance" "instance" {
+  name             = "my-database-instancexxxx"
+  region           = var.gcp_region
+  database_version = "MYSQL_8_0"
+  settings {
+    tier = "db-f1-micro"
+  }
+
+  deletion_protection  = "true"
+}
+
+resource "google_sql_database" "database" {
+  name     = "my-database"
+  instance = google_sql_database_instance.instance.name
 }
